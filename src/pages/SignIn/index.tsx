7d18157c69a96d4c8ea386b847b05433ffdb5ee1
Torres <sergio.uft@gmail.com>
2020-07-02 14:08:40 -0300

    feat: conform SignIn page with unform

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import {
   View,
   Image,
@@ -7,7 +7,8 @@ import {
   Platform,
 } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-
+import { Form } from '@unform/mobile';
+import { FormHandles } from '@unform/core';
 import Icon from 'react-native-vector-icons/Feather';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
@@ -23,8 +24,18 @@ import {
 
 import logoImg from '../../assets/logo.png';
 
+interface UserSignIn {
+  email: string;
+  password: string;
+}
+
 const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
   const navigation = useNavigation();
+
+  const handleSignIn = useCallback((data: UserSignIn) => {
+    console.log(data);
+  }, []);
 
   return (
     <>
@@ -43,11 +54,19 @@ const SignIn: React.FC = () => {
               <Title>Log In</Title>
             </View>
 
-            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Form
+              style={{ width: '100%' }}
+              ref={formRef}
+              onSubmit={handleSignIn}
+            >
+              <Input name="email" icon="mail" placeholder="E-mail" />
 
-            <Input name="password" icon="lock" placeholder="Password" />
+              <Input name="password" icon="lock" placeholder="Password" />
 
-            <Button>Log in</Button>
+              <Button onPress={() => formRef.current?.submitForm()}>
+                Log in
+              </Button>
+            </Form>
 
             <ForgotPassword>
               <ForgotPasswordText>Forgot my password</ForgotPasswordText>
