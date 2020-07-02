c32f996529a8d09ba695f0e9ad78444792239392
Torres <sergio.uft@gmail.com>
2020-07-02 16:51:18 -0300

    feat: implement handleSignIn on SignIn page

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -6,11 +6,14 @@ import {
   ScrollView,
   Platform,
   TextInput,
+  Alert,
 } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
 import Icon from 'react-native-vector-icons/Feather';
+import getValidationErrors from '../../util/getValidationErrors';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 
@@ -25,7 +28,7 @@ import {
 
 import logoImg from '../../assets/logo.png';
 
-interface UserSignIn {
+interface SignInFormData {
   email: string;
   password: string;
 }
@@ -36,8 +39,34 @@ const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
   const passwordInputRef = useRef<TextInput>(null);
 
-  const handleSignIn = useCallback((data: UserSignIn) => {
-    console.log(data);
+  const handleSignIn = useCallback(async (data: SignInFormData) => {
+    try {
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required('Email is required')
+          .email('Please inform a valid email'),
+        password: Yup.string().required('Password is required'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      if (err instanceof Yup.ValidationError) {
+        const errors = getValidationErrors(err);
+
+        formRef.current?.setErrors(errors);
+
+        return;
+      }
+
+      Alert.alert(
+        'Authentication error',
+        'An error occurred while trying to authenticate, please try again',
+      );
+    }
   }, []);
 
   return (
