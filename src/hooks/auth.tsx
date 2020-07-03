2cbbbd58765a5cc128c70c8ee351ab989cc3b3bd
Torres <sergio.uft@gmail.com>
2020-07-03 14:07:17 -0300

    feat: automatically include header with token on API calls when user is authenticated

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -46,6 +46,8 @@ const AuthProvider: React.FC = ({ children }) => {
       ]);
 
       if (token[1] && user[1]) {
+        api.defaults.headers.authorization = `Bearer ${token[1]}`;
+
         setData({ token: token[1], user: JSON.parse(user[1]) });
       }
 
@@ -68,7 +70,7 @@ const AuthProvider: React.FC = ({ children }) => {
       ['@GoBarber:user', JSON.stringify(user)],
     ]);
 
-    // api.defaults.headers.authorization = `Bearer ${token}`;
+    api.defaults.headers.authorization = `Bearer ${token}`;
 
     setData({ token, user });
   }, []);
