28e63d3546063141f78d3d60f60db66dafa06698
Torres <sergio.uft@gmail.com>
2020-07-04 17:29:53 -0300

    feat: implement Profile page

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,8 +1,141 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useCallback, useRef } from 'react';
+import {
+  Alert,
+  View,
+  KeyboardAvoidingView,
+  ScrollView,
+  Platform,
+  TextInput,
+} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { Form } from '@unform/mobile';
+import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
+import Icon from 'react-native-vector-icons/Feather';
+import { useAuth } from '../../hooks/auth';
+import getValidationErrors from '../../util/getValidationErrors';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+import api from '../../services/api';
+
+import {
+  Container,
+  Title,
+  BackButton,
+  UserAvatarButton,
+  UserAvatar,
+} from './styles';
+
+interface ProfileFormData {
+  name: string;
+  email: string;
+  old_password: string;
+  password?: string;
+  password_confirmation?: string;
+}
 
 const Profile: React.FC = () => {
-  return <View />;
+  const { user } = useAuth();
+
+  const formRef = useRef<FormHandles>(null);
+  const navigation = useNavigation();
+
+  const emailInputRef = useRef<TextInput>(null);
+  const OldPasswordInputRef = useRef<TextInput>(null);
+  const passwordInputRef = useRef<TextInput>(null);
+  const ConfirmPasswordInputRef = useRef<TextInput>(null);
+
+  const handleGoBack = useCallback(() => {
+    navigation.goBack();
+  }, [navigation]);
+
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          // contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <BackButton onPress={handleGoBack}>
+              <Icon name="chevron-left" size={24} color="#999591" />
+            </BackButton>
+            <UserAvatarButton onPress={() => {}}>
+              <UserAvatar source={{ uri: user.avatar_url }} />
+            </UserAvatarButton>
+            <View>
+              <Title>My profile</Title>
+            </View>
+
+            <Form style={{ width: '100%' }} ref={formRef} onSubmit={() => {}}>
+              <Input
+                autoCapitalize="words"
+                name="name"
+                icon="user"
+                placeholder="Name"
+                returnKeyType="next"
+                onSubmitEditing={() => emailInputRef.current?.focus()}
+              />
+
+              <Input
+                ref={emailInputRef}
+                keyboardType="email-address"
+                autoCorrect={false}
+                autoCapitalize="none"
+                name="email"
+                icon="mail"
+                placeholder="E-mail"
+                returnKeyType="next"
+                onSubmitEditing={() => OldPasswordInputRef.current?.focus()}
+              />
+
+              <Input
+                ref={OldPasswordInputRef}
+                name="old_password"
+                icon="lock"
+                placeholder="Password"
+                secureTextEntry
+                containerStyle={{ marginTop: 16 }}
+                textContentType="newPassword"
+                returnKeyType="next"
+                onSubmitEditing={() => passwordInputRef.current?.focus()}
+              />
+
+              <Input
+                ref={passwordInputRef}
+                name="password"
+                icon="lock"
+                placeholder="New password"
+                secureTextEntry
+                textContentType="newPassword"
+                returnKeyType="next"
+                onSubmitEditing={() => ConfirmPasswordInputRef.current?.focus()}
+              />
+
+              <Input
+                ref={ConfirmPasswordInputRef}
+                name="password_confirmation"
+                icon="lock"
+                placeholder="New password confirmation"
+                secureTextEntry
+                textContentType="newPassword"
+                returnKeyType="send"
+                onSubmitEditing={() => formRef.current?.submitForm()}
+              />
+
+              <Button onPress={() => formRef.current?.submitForm()}>
+                Confirm changes
+              </Button>
+            </Form>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </>
+  );
 };
 
 export default Profile;
