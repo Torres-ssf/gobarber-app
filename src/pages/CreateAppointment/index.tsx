f37c8dc93a83d400a5a803cf4fc93804cede91d1
Torres-ssf <sergio.uft@gmail.com>
2020-09-05 17:06:32 -0300

    feat: alert network connection error at CreateAppointment page

--- src/pages/CreateAppointment/index.tsx
+++ src/pages/CreateAppointment/index.tsx
@@ -85,6 +85,13 @@ const CreateAppointment: React.FC = () => {
             'Your session has expired, please sign in again',
           );
         }
+
+        if (err.message && err.message === 'Network Error') {
+          Alert.alert(
+            'Account creation error',
+            'An error occurred while trying to get providers list, check your network connection',
+          );
+        }
       }
     };
 
