45674f7fbce7e3ed9e30489f4afe02c6242d2c02
Torres-ssf <sergio.uft@gmail.com>
2021-03-02 20:19:42 -0300

    chore(types): define react-native-dotenv variables

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,1 +1,8 @@
 declare module '*.png';
+
+declare module 'react-native-dotenv' {
+  export const APP_ENV: 'dev' | 'prod';
+  export const API_HOST_IOS: string;
+  export const API_HOST_ANDROID: string;
+  export const API_PROD_HOST: string;
+}
