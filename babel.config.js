f6d58ac90dcf515368dd060e21ba64772b07a91e
Torres-ssf <sergio.uft@gmail.com>
2021-03-02 20:18:02 -0300

    chore(babel): add config for dotenv

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,9 @@
 module.exports = {
-  presets: [
-    'module:metro-react-native-babel-preset', 
-    'module:react-native-dotenv'
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    ['module:react-native-dotenv', {
+        moduleName: 'react-native-dotenv',
+      },
+    ],
   ],
 };
